29c56140c0485feb9cffeeec86d82e59443ec6f2
AgilityLars <southparkourify@gmail.com>
2018-04-04 13:30:04 +0200

    Pause Menu
    Added pause menu to the game. Using the script "MenuAppearScript" you first decide which canvas to show from the start and then you choose which canvas to hide on default. You will then switch between the canvas's on button click when using the button you decide.

---
+++ Assets/Scripts/MenuAppearScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAppearScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject menuToShow;
+    [SerializeField] private GameObject menuToHide;
+    [SerializeField] private string button;
+    private bool isShowing = true;
+
+    void Start()
+    {
+        menuToShow.SetActive(true);
+        menuToHide.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(button))
+        {
+            isShowing = !isShowing;
+            menuToShow.SetActive(isShowing);
+            menuToHide.SetActive(!isShowing);
+        }
+    }
+}
