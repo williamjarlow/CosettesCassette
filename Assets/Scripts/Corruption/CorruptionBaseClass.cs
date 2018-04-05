4a095ea1db397dd3e7cecd01b5b50e184ea934a4
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2018-04-05 13:55:26 +0200

    Corruption
    Corruption base class, corruption handler and a basic example of how they can be used

---
+++ Assets/Scripts/Corruption/CorruptionBaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CorruptionBaseClass : MonoBehaviour {
+    [HideInInspector]
+    public Duration duration;
+    [HideInInspector]
+    public bool corruptionCleared;
+
+    virtual public void StartCorruption() { }
+    virtual public void EndCorruption() { }
+}
