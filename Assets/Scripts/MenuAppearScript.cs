f8ef0585db86c095bd7467c12eb0684a3f766148
AgilityLars <southparkourify@gmail.com>
2018-04-05 14:05:54 +0200

    Unpause game through touch
    Added so we can unpause the game with touchbuttons. You need to assign which button specifically through the MenuAppearScript in order to get it to work.

--- Assets/Scripts/MenuAppearScript.cs
+++ Assets/Scripts/MenuAppearScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuAppearScript : MonoBehaviour
@@ -6,18 +7,21 @@ public class MenuAppearScript : MonoBehaviour
 
     [SerializeField] private GameObject menuToHide;
     [SerializeField] private GameObject menuToShow;
-    [SerializeField] private string button;
+    [SerializeField] private string keyboardButton;
+    [SerializeField] private Button UIbutton;
     private bool isShowing = true;
 
     void Start()
     {
         menuToShow.SetActive(true);
         menuToHide.SetActive(false);
+        Button btn = UIbutton.GetComponent<Button>();
+        btn.onClick.AddListener(() => Pause());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(button))
+        if (Input.GetKeyDown(keyboardButton))
         {
             if (Time.timeScale == 0.0F)
             {
@@ -31,5 +35,20 @@ public class MenuAppearScript : MonoBehaviour
             menuToShow.SetActive(isShowing);
             menuToHide.SetActive(!isShowing);
         }
+    }
+
+    void Pause()
+    {
+        if (Time.timeScale == 0.0F)
+        {
+            Time.timeScale = 1.0F;
+        }
+        else
+        {
+            Time.timeScale = 0.0F;
+        }
+        isShowing = !isShowing;
+        menuToShow.SetActive(isShowing);
+        menuToHide.SetActive(!isShowing);
     }
 }
