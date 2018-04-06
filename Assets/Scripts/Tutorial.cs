2739df8ef91bb517a93565fd807bf43ca5ec982c
AgilityLars <southparkourify@gmail.com>
2018-04-06 11:11:06 +0200

    Tutorial Prefab
    Added a turotial prefab to a new test scene. The tutorial prefab needs to be filled with the correct sprites before using.
    
    To start the prefab, simply call the function TutorialEvent(int) whereas the int is the index of which tutorial you want to show.

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    [SerializeField] private GameObject tutorialPrefab;
+    [SerializeField] private Sprite[] tutorialSprite;
+    [SerializeField] private Button tutButton;
+
+    void Start()
+    {
+        //Change base value of the chosen sprite's alpha to fit desired esthetic effect
+        Image tutorialImage = tutorialPrefab.GetComponent<Image>();
+        Color tutAlpha = tutorialImage.color;
+        tutAlpha.a = 0.75f;
+        tutorialImage.color = tutAlpha;
+
+        //Add Listener to the button to leave the tutorial 
+        Button tutorialButton = tutButton.GetComponent<Button>();
+        tutorialButton.onClick.AddListener(() => Resume());
+
+        tutorialPrefab.SetActive(false);
+    }
+
+    //Stops tutorial and resumes time
+    void Resume()
+    {
+        Time.timeScale = 1.0F;
+        tutorialPrefab.SetActive(false);
+    }
+
+    //Call this funciton when you want to show tutorial with which sprite you want to use
+    public void TutorialEvent(int tutorialIndex)
+    {
+        tutorialImage.sprite = tutorialSprite[tutorialIndex];
+        Time.timeScale = 0.0F;
+        tutorialPrefab.SetActive(true);
+    }
+}
