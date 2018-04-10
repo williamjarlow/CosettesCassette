e4f858c90d2c2d6e25c699956bd57aa13d7fcec7
Henrik <a16henle@student.his.se>
2018-04-10 16:33:45 +0200

    Visual fixes, optimizations
    Visual input system ready for tap effect.
    Optimized particle system with particle materials for mobile platforms.

--- Assets/Scripts/VisualInput.cs
+++ Assets/Scripts/VisualInput.cs
@@ -4,58 +4,93 @@ using UnityEngine;
 
 public class VisualInput : MonoBehaviour {
 
+    [Header("Particle Effects")]
+    [SerializeField] private GameObject trailParticleEffect;
+    [SerializeField] private GameObject touchParticleEffect;
+    [Tooltip("Minimum allowed distance for a tap to trigger instead of movement.")]
+    [SerializeField] private float minAllowedDistForTap = 0.25f;
+    [SerializeField] private float distanceFromCamera = 5;
+    [Header("Trail effect")]
+    [SerializeField] private float speed = 25;
+    [Header("Tap effect")]
+    [Tooltip("Amount of times to emit effect")]
+    [SerializeField] private int emitCount = 1;
 
-    public GameObject particleEffect;
-    public float speed = 25;
-    public float distanceFromCamera = 5;
+    private Vector3 origPosition = Vector3.zero;
 
     // Temporary
-    [Header("For Testing Purposes")]
-    public bool workWithMouseInput = false;
+    ////
+    [Header("For testing purposes, activate before starting")]
+    [SerializeField] private bool workWithMouseInput = false;
+    ////
 
 
     void Start ()
     {
+        // Temporary
+        ////
         if (workWithMouseInput)
-            particleEffect.SetActive(true);
-	}
+            trailParticleEffect.SetActive(true);
+        ////
+    }
 
 
 
     void Update ()
     {
+        // Temporary
+        ////
         if (workWithMouseInput)
         FollowMouse();
         if (!workWithMouseInput)
+        ////
         FollowTouch();
     }
 
     void FollowTouch()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0)                       // Do we have input?
         {
             Touch myTouch = Input.touches[0];
 
             Vector3 touchPosition = myTouch.position;
             touchPosition.z = distanceFromCamera;
 
+
+
             if (myTouch.phase == TouchPhase.Began)
             {
+                origPosition = transform.position;      // Store position where finger started
                 transform.position = Camera.main.ScreenToWorldPoint(touchPosition);
-                particleEffect.SetActive(true);
+                trailParticleEffect.SetActive(true);
             }
 
+            if (myTouch.phase == TouchPhase.Moved)
+            { 
             Vector3 touchScreenToWorld = Camera.main.ScreenToWorldPoint(touchPosition);
-
             Vector3 position = Vector3.Lerp(transform.position, touchScreenToWorld, 1 - Mathf.Exp(-speed * Time.deltaTime));
-
             transform.position = position;
+            }
+
+            if (myTouch.phase == TouchPhase.Ended && Vector3.Distance(origPosition, transform.position) < minAllowedDistForTap) {
+                // Ripple effect!
+                // touchParticleEffect.SetActive(true);
+
+                //touchParticleEffect.GetComponent<ParticleSystem>().Play();
+                // DO MORE STAFFZ MAYBE?!?!?!?!?
+                touchParticleEffect.GetComponent<ParticleSystem>().Emit(emitCount);
+
+                //touchParticleEffect.SetActive(false);
+                trailParticleEffect.SetActive(false);
+            }
 
             if (myTouch.phase == TouchPhase.Ended)
-                particleEffect.SetActive(false);
+                trailParticleEffect.SetActive(false);
         }
     }
 
+    // Temporary
+    ////
     void FollowMouse() 
     {
         Vector3 mousePosition = Input.mousePosition;
@@ -66,6 +101,13 @@ public class VisualInput : MonoBehaviour {
         Vector3 position = Vector3.Lerp(transform.position, mouseScreenToWorld, 1 - Mathf.Exp(-speed * Time.deltaTime));
 
         transform.position = position;
+
+
+        // DO MORE STAFFZ MAYBE?!?!?!?!?
+        if (Input.GetMouseButtonDown(0))
+            touchParticleEffect.GetComponent<ParticleSystem>().Emit(emitCount);
+
     }
+    ////
 
 }
