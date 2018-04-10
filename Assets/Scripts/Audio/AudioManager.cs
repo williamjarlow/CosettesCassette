39831c547811953875d22e34e9922e2463850b0d
Hannes <simesgaarden@hotmail.com>
2018-04-10 15:09:18 +0200

    FMOD Banks and Drum Mechanic
    Fixed loading banks and first draft of the drum mechanic, eg. being able to record and listen to your recording.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class AudioManager : MonoBehaviour {
 
@@ -10,20 +12,51 @@ public class AudioManager : MonoBehaviour {
     private AudioMusic audioMusic;
 	private AudioDistortion audioDistortion;
     private FMOD.Studio.EventDescription musicEventDesc;
+    private FMOD.RESULT result;
+    FMOD.Studio.System systemObj;
+    FMOD.System lowLevelSys;
 
     private int trackLength;
     [SerializeField] private string musicPath;
     [SerializeField] private string audioLogPath;
+    //Bank files to load, should only be the file name in the directory, eg. 'Cassette_01.bank'
+    [SerializeField] private List<string> bankFiles;
 
-	void Start (){
-		audioMusic = GetComponent<AudioMusic> ();
-		audioDistortion = GetComponent<AudioDistortion> ();
 
+
+    void Awake (){
+
+        Debug.Assert(bankFiles.Count > 0, "Enter the bank file names into the audio manager");
+
+        audioDistortion = GetComponent<AudioDistortion> ();
+
+        //Attempts to reduce sound delay
+        result = FMOD.Studio.System.create(out systemObj);
+        result = systemObj.getLowLevelSystem(out lowLevelSys);
+        result = lowLevelSys.setDSPBufferSize(512, 4);
+        Debug.Log(result);
+        result = lowLevelSys.setSoftwareFormat(44100, FMOD.SPEAKERMODE.DEFAULT, 0);
+        result = lowLevelSys.setOutput(FMOD.OUTPUTTYPE.OPENSL);
+        result = systemObj.initialize(64, FMOD.Studio.INITFLAGS.NORMAL, FMOD.INITFLAGS.NORMAL, (IntPtr)null);
+
+
+        //Loads the banks 
+        for (int i = 0; i < bankFiles.Count; i++)
+        {
+            FMODUnity.RuntimeManager.LoadBank(bankFiles[i], true);
+        }
+
+        //Get the event description, needed to get Track Length
         musicEventDesc = FMODUnity.RuntimeManager.GetEventDescription(musicPath);
         musicEventDesc.getLength(out trackLength);
+
+        //Destroy systemObj because one is created each time in 'play' mode, and the unity editor does not destroy it for you when exiting 'play' mode
+        systemObj.release();
+        lowLevelSys.release();
+
     }
 
-	public void AudioPlayMusic (string musicTrack){
+	public void AudioPlayMusic (){
         gameMusicEv = FMODUnity.RuntimeManager.CreateInstance(musicPath);
         gameMusicEv.start();
     }
---
+++ Assets/Scripts/DrumMechanic.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script may be transported to AudioTimeline
+
+public class DrumMechanic : MonoBehaviour {
+
+    /*[HideInInspector]*/ public List<int> inputTimeStamps;
+
+    private AudioManager audioManager;
+    private int timeStamp;
+    private bool recording = false;
+    [SerializeField] private int iterator = 0;
+    [SerializeField] private string bassDrumPath;
+
+
+    void Start ()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+
+        Debug.Assert(audioManager != null, "Could not find the Audio Manager");
+	}
+
+
+    void Update()
+    {
+        //Find the timeline position in the track
+        audioManager.gameMusicEv.getTimelinePosition(out timeStamp);
+
+        if (recording)
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && timeStamp > 0 || Input.GetMouseButtonDown(0))
+            {
+                FMODUnity.RuntimeManager.PlayOneShot(bassDrumPath);
+                inputTimeStamps.Add(timeStamp);
+                Debug.LogError("beat");
+            }
+        }
+
+
+        else if (recording == false)
+        {
+            //  Make sure the list is not empty and check if timeStamp matches the value in the list[i]. Also check if we are at the last index in the list
+            if (inputTimeStamps.Count > 0 && timeStamp == inputTimeStamps[iterator] && iterator < inputTimeStamps.Count - 1)
+            {
+                FMODUnity.RuntimeManager.PlayOneShot(bassDrumPath);
+                iterator++;
+            }
+        }
+
+
+    }
+        public void Record()
+    {
+        //  TODO: Prompt user that the previous recording (if one exists) will be deleted
+        //  Delete previous recording if user confirms, do nothing if not
+
+        inputTimeStamps.Clear();
+        iterator = 0;
+
+        recording = true;
+    }
+
+    public void Listen()
+    {
+        recording = false;
+    }
+}
