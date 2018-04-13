7c5164825fbb0e7937ca6456e9369f1398247c74
Hannes <simesgaarden@hotmail.com>
2018-04-13 14:25:53 +0200

    Corruption Visuals (unfinished)
    Hardcoded corruption visuals for the build.

---
+++ Assets/Scripts/Corruption/CorruptionVisual_TEST.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorruptionVisual_TEST : MonoBehaviour {
+
+    private RectTransform corruptedArea;
+
+    private AudioManager audioManager;
+
+	void Start ()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test_Folder/CorruptionVisuals.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+
+public class CorruptionVisuals : MonoBehaviour {
+
+    public List<Rect> rectList;
+    public List<Texture> imageList;
+
+
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    { 
+        GUI.Box(rectList[0], imageList[0]);
+
+    }
+}
