edf58f8a99f1167b71dca7a447f40fb17d969b9e
Jung-bin <marcus.lagstrom@gmail.com>
2018-04-13 14:55:22 +0200

    Credits
    I made credits that move

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject stopper;
+    [SerializeField] private float endPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        endPos = stopper.transform.position.y;
+
+        		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        
+        if (transform.position.y < endPos)
+        {
+            transform.Translate(Vector3.up * speed);
+          
+        }
+        else
+        {
+            transform.Translate(Vector3.up * 0);
+            
+        }
+    }
+}
