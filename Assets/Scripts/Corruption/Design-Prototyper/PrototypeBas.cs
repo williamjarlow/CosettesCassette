6b32d87d5330634c9739562f1e766a7c0101ff01
Morgeth <melker2010@live.com>
2018-04-17 15:20:28 +0200

    Update prep for Bass test
    Stuff

---
+++ Assets/Scripts/Corruption/Design-Prototyper/PrototypeBas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeBas : MonoBehaviour {
+    bool corruptionstarted;
+
+    [Header("Damage and Gain per segment")]
+    private int Perfectrange;
+    [SerializeField] [Range(0,1)] private int missDamage;
+    [SerializeField] [Range(0,1)]private int hitGain;
+
+    [Header("SuccessRanges")]
+    public int okayRange;
+    public int perfectRange;
+
+    public List<int> Segment;
+    DrumCorruptionHandler drumCorruptionHandler;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Corruption/Design-Prototyper/PrototypeBasHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BasInformation
+{
+    [Header("Pitch Settings")]
+    public int StartingPitch;
+    [Range(1, 10)] public int MaxMinPitch; 
+    [Header("ID of segment in 'Game Manager'")]
+    public int segmentID;
+}
+public class PrototypeBasHandler : MonoBehaviour {
+
+    [SerializeField] [Tooltip("Amount of Bas corruptions, as well as their information.")]
+    List<BasInformation> BasList;
+
+    [SerializeField] GameObject basCorruptionPrefab;
+
+    OverallCorruption overallCorruption;
+    // Use this for initialization
+    void Start () {
+        overallCorruption = GetComponent<OverallCorruption>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
