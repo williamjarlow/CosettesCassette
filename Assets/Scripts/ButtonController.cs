b4962757670928f43d8e7ec25b5bfbd228f80ff3
Morgeth <melker2010@live.com>
2018-04-18 15:14:36 +0200

    Added scripts to manipulate ui buttons through the 3d buttons on the cassetteplayer.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+    [SerializeField] GameObject UITarget;
+
+    [SerializeField] private bool isDual;
+    [SerializeField] GameObject UITargetDual;
+    private enum buttonstates { Up, Down, Moving }
+    buttonstates buttonstate;
+    bool startingPos;
+
+    private Vector3 Startingposition;
+    [SerializeField] float buttonPressdepth;
+    private Vector3 endposition;
+    [SerializeField] private int buttonSpeed;
+    private bool buttonclicked = false;
+    // Use this for initialization
+    void Start () {
+        buttonstates buttonstate;
+        buttonstate = buttonstates.Up;
+        Startingposition = transform.position;
+        endposition = new Vector3(Startingposition.x, Startingposition.y, Startingposition.z + buttonPressdepth);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        switch (buttonstate)
+        {
+            //Move buttons up or down
+            case buttonstates.Up:
+                break;
+            case buttonstates.Moving:
+                if (startingPos)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, endposition, buttonSpeed);
+                    if (transform.position == endposition) buttonstate = buttonstates.Down;
+                }
+                else if (!startingPos)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, Startingposition, buttonSpeed);
+                    if (transform.position == Startingposition) buttonstate = buttonstates.Up;
+                }
+                break;
+            case buttonstates.Down:
+                if (!isDual)
+                {
+                    startingPos = false;
+                    buttonstate = buttonstates.Moving;
+                }
+                else { }
+                break;
+        }
+	}
+    private void OnMouseOver()
+    {
+        if (buttonstate != buttonstates.Moving)
+        {
+
+
+            if (!isDual)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    UITarget.GetComponent<Button>().onClick.Invoke();
+                    buttonstate = buttonstates.Moving;
+                    startingPos = true;
+                }
+            }
+            else if (isDual)
+            {
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (buttonclicked)
+                    {
+                        UITargetDual.GetComponent<Button>().onClick.Invoke();
+                        buttonclicked = false;
+                        startingPos = false;
+                        buttonstate = buttonstates.Moving;
+                    }
+                    else if (!buttonclicked)
+                    {
+                        UITarget.GetComponent<Button>().onClick.Invoke();
+                        buttonclicked = true;
+                        startingPos = true;
+                        buttonstate = buttonstates.Moving;
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
