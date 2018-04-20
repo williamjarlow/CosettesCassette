c8383d37afb35ad1933676ce33355176cab6d993
Jung-bin <marcus.lagstrom@gmail.com>
2018-04-20 11:55:28 +0200

    Small Credit fix
    Small credit fixes

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
 
 public class Credits : MonoBehaviour {
 
     [SerializeField] private GameObject stopper;
+    [SerializeField] private float origPos;
     [SerializeField] private float currentPos;
     [SerializeField] private float endOfTheLine;
     [SerializeField] private GameObject AudioManagerz;
+    [SerializeField] private GameObject credits;
     private AudioManager audioManager;
     private float audiolength;
     private float audioPos;
@@ -18,21 +22,39 @@ public class Credits : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        
         audioManager = AudioManagerz.GetComponent<AudioManager>();
+        origPos = 1100f;
         endOfTheLine = 2260.116f;
         audiolength = audioManager.GetTrackLength();
+        //FindString();
+        //Text temp = credits.GetComponent<Text>();
+        //temp.text = FindString();
+        
     }
 
     // Update is called once per frame
     void Update ()
     {
-
         audioPos = audioManager.GetTimeLinePosition();
         audioP = audioPos / audiolength;
         currentPos = audioP * endOfTheLine;
         Vector3 temp = transform.localPosition;
-        temp.y = currentPos - 1100;
+        temp.y = currentPos - origPos;
         transform.localPosition = temp;
 
     }
+
+    /*static string FindString()
+    {
+        string path = "Assets/Test_Folder/Credits.txt";
+
+        StreamReader creditstext = new StreamReader(path);
+        Debug.Log(creditstext.ReadToEnd());
+        creditstext.Close();
+        return creditstext.ToString();
+
+    }*/
+  
 }
+
