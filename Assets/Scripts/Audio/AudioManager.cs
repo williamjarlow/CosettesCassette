4fd1d029dba891b195c69f3b77e3ca09355b4d4f
Jack Lindgren <38013685+jacklindgren24@users.noreply.github.com>
2018-04-20 16:36:48 +0200

    AudioManager update (DSP)
    Added function to retrieve pitch shift DSPs, play/pause sounds.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -22,7 +22,6 @@ public class AudioManager : MonoBehaviour {
     [SerializeField] private string audioLogPath;
     public string bassDrumPath;
 
-    
     [Tooltip("Bank files to load, should only be the file name in the directory, eg. 'Cassette_01.bank'")]
     [SerializeField] private List<string> bankFiles;
 
@@ -57,7 +56,6 @@ public class AudioManager : MonoBehaviour {
         FMOD.Studio.System.create(out systemObj);
         systemObj.getLowLevelSystem(out lowLevelSys);
 
-
         //FMOD system object initialization
         lowLevelSys.setSoftwareFormat(44100, FMOD.SPEAKERMODE.DEFAULT, 0);
         lowLevelSys.setDSPBufferSize(512, 4);
@@ -73,7 +71,10 @@ public class AudioManager : MonoBehaviour {
 
         if(switchedToAudioLog)
             gameMusicEv = FMODUnity.RuntimeManager.CreateInstance(audioLogPath);
+		
         gameMusicEv.start();
+
+		StartCoroutine (GetDSP());
     }
 
 	public void AudioStopMusic ()
@@ -83,13 +84,48 @@ public class AudioManager : MonoBehaviour {
 
 	public void AudioPauseMusic ()
     {
+		FMODUnity.RuntimeManager.PlayOneShot ("event:/Interface/Playback/pause");
         gameMusicEv.setPaused(true);
     }
 
 	public void AudioUnpauseMusic ()
     {
+		FMODUnity.RuntimeManager.PlayOneShot ("event:/Interface/Playback/play");
         gameMusicEv.setPaused(false);
     }
+
+	private IEnumerator GetDSP()
+	{
+		FMOD.ChannelGroup musicChanGroup;
+		FMOD.ChannelGroup musicChanSubGroup;
+
+		FMOD.DSPConnection DSPCon;
+
+		FMOD.DSP musicChanSubGroupDSP;
+		FMOD.DSP pitchChordsDSP;
+		FMOD.DSP pitchVocalsDSP;
+		FMOD.DSP pitchDrumsDSP;
+		FMOD.DSP pitchBassDSP;
+		FMOD.DSP pitchLeadDSP;
+
+		yield return new WaitForSeconds(0.5f); // Gives event time to load (better solution needed)
+		result = gameMusicEv.getChannelGroup (out musicChanGroup);
+
+		result = musicChanGroup.getGroup (0, out musicChanSubGroup);
+		result = musicChanSubGroup.getDSP (3, out musicChanSubGroupDSP); // 3 carries channels of instrument tracks, 0 is the sum track
+		result = musicChanSubGroupDSP.getInput (0, out pitchChordsDSP, out DSPCon); // adding effects in studio messes up 4 and beyond
+		result = musicChanSubGroupDSP.getInput (1, out pitchVocalsDSP, out DSPCon);
+		result = musicChanSubGroupDSP.getInput (2, out pitchDrumsDSP, out DSPCon);
+		result = musicChanSubGroupDSP.getInput (3, out pitchBassDSP, out DSPCon);
+		result = musicChanSubGroupDSP.getInput (4, out pitchLeadDSP, out DSPCon);
+
+		//in DSP 3 of subgroup
+		//0 = chords
+		//1 = vocals
+		//2 = drums
+		//3 = bass
+		//4 = lead
+	}
 
     public float GetTrackLength()
     {
