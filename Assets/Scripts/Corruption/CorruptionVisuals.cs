fdf517b75c2b0d14d965e051ec1f32614826e819
Hannes <simesgaarden@hotmail.com>
2018-04-20 10:50:07 +0200

    Corruption Visuals connected to actual corruption
    Fixed integration with William's corruption script so that the corruption visuals are automatically placed where the corrupted areas are

--- Assets/Scripts/Corruption/CorruptionVisuals.cs
+++ Assets/Scripts/Corruption/CorruptionVisuals.cs
@@ -9,58 +9,48 @@ public class CorruptionVisuals : MonoBehaviour {
 
     private AudioManager audioManager;
 
-    private Transform timelineObject;
     private Image maskImage;
     private Color originalColor;
     private RectTransform timelineRectTransform;
     private RectTransform corruptedAreaRectTransform;
 
-
     [Tooltip("Converts milliseconds to pixels, ie. how many milliseconds one pixel represents")] private float pixelToMs;
     [Tooltip("The original corrupted area rect width")] private float originalCorruptedRectWidth;
-    [Tooltip("Start of the corruption in milliseconds")][SerializeField] private float corruptionStart;
-    [Tooltip("End of the corruption in milliseconds")] [SerializeField] private float corruptionEnd;
+    [Tooltip("Start of the corruption in milliseconds")] private float corruptionStart;
+    [Tooltip("End of the corruption in milliseconds")] private float corruptionEnd;
     private float rectWidth;
     private float rectHeight;
     private int trackLength;
 
-    // *** TODO ***
-    // Connect the values 'corruptionStart' and 'corruptionEnd' to William's corruption script. Currently they are just values not connected to anything.
 
     void Start ()
     {
-
-        // Initialize variables
-        timelineObject = gameObject.transform.parent;
+        // Save the original color to be able to reset it when the corrupted area has been fixed
         maskImage = GetComponentInChildren<Image>();
+        originalColor = maskImage.color;
+
+	}
+
+    public void SetCorruptionPosition(int corruptionStartPoint, int corruptionEndPoint)
+    {
+        // Initialize variables
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
-        timelineRectTransform = timelineObject.GetComponent<RectTransform>();
         corruptedAreaRectTransform = gameObject.GetComponent<RectTransform>();
         originalCorruptedRectWidth = corruptedAreaRectTransform.rect.width;
+        timelineRectTransform = gameObject.transform.parent.GetComponent<RectTransform>();
         trackLength = (int)audioManager.GetTrackLength();
-
-        // Make sure the game object has a parent
-        Debug.Assert(timelineObject != null, "Attach the corruption visual prefab as a child of Timeline Slider");
-
 
         // Calculate how many milliseconds one pixel represents
         rectWidth = timelineRectTransform.rect.width;
         pixelToMs = trackLength / rectWidth;
 
-        // Save the original color to be able to reset it when the corrupted area has been fixed
-        originalColor = maskImage.color;
-
-	}
-
-    public void SetCorruptionPosition(float corruptionStartPoint, float corruptionEndPoint)
-    {
         // Convert start- and endpoint values to pixel values
         corruptionStart = corruptionStartPoint / pixelToMs;
         corruptionEnd = corruptionEndPoint / pixelToMs;
 
         // Change the width of the corrupted area according to the pixel-to-ms values
         corruptedAreaRectTransform.sizeDelta = new Vector2(corruptionEnd - corruptionStart, timelineRectTransform.rect.height);
-
+        
         /* 1. Place the anchor (middle) of the corrupted object at the start of the timeline position
          * 2. Place the start of the corrupted object at the start of the timeline position
          * 3. Place the start of the corrupted object at the start of the corruptionStart variable  */
