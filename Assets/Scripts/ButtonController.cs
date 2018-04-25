70fd087eb25ee4ff013bcf14fed40c427f445cc3
Morgeth <melker2010@live.com>
2018-04-25 14:56:20 +0200

    LevelSynced&ButtonsReady
    - Finished layout of the corruptions
    - Movement to buttons
    - Temporary change on isolation button into record button
    - Timing checked.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -96,4 +96,10 @@ public class ButtonController : MonoBehaviour {
         }
 
     }
+    public void setButtonEnabled()
+    {
+        if (buttonclicked) { buttonclicked = false; startingPos = false; buttonstate = buttonstates.Moving; }
+        else if (!buttonclicked) { buttonclicked = true; startingPos = true; buttonstate = buttonstates.Moving; }
+
+    }
 }
