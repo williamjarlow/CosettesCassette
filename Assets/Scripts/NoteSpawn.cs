a053eb13b020a031bfb678ae4d5799dddc7e707a
Jung-bin <marcus.lagstrom@gmail.com>
2018-05-03 14:35:40 +0200

    DuckHunt
    Start of Duck Hunt mechanic. Can spawn blocks that move up. The amount and the speed they move can be changed from the inspector

---
+++ Assets/Scripts/NoteSpawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingStuff : MonoBehaviour {
+
+
+    public Vector3 gravity;
+    //public Rigidbody rb;
+    [SerializeField] private GameObject ducks;
+    [SerializeField] private int duckAmount;
+    //[SerializeField] private GameObject canvas;
+    //[SerializeField] private GameObject spawn;
+    [SerializeField] private float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+
+        //rb = GetComponent<Rigidbody>();
+        //rb.AddForce(gravity, ForceMode.Impulse);
+        Fill();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+        
+    }
+
+    public void Fill()
+    {
+        //duckies = new GameObject[ducks.Length];
+        for (int i = 0; i < duckAmount; i++)
+        {
+            gravity.x = Random.Range(-5, 5);
+            gravity.y = Random.Range(-5, 5);
+            ducks = Instantiate(ducks, new Vector3(gravity.x,gravity.y,0), Quaternion.identity);//spawn.GetComponent<Transform>().transform);//canvas.GetComponent<Canvas>().transform);
+            ducks.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.up * speed);    
+            //duckies[i].transform.parent = GameObject.Find("Canvas").transform;
+        }
+    }
+
+}
