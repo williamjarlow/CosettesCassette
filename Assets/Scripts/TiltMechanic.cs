ad29a2874aaf321dbaa1f85504360e9f823ad4b4
AgilityLars <southparkourify@gmail.com>
2018-05-03 10:53:59 +0200

    TiltMechanic test

---
+++ Assets/Scripts/TiltMechanic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltMechanic : MonoBehaviour {
+
+    [SerializeField] private float moveMusic;
+    [SerializeField] GameObject audioMan;
+    private AudioManager am;
+    FMOD.RESULT result;
+
+	// Use this for initialization
+	void Start () {
+        am = audioMan.GetComponent<AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //am.musicChanSubGroup.setPan(moveMusic);
+        am.musicChanGroup.setPan(moveMusic);
+    }
+}
