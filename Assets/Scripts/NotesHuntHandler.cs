3ef5111cae7a5a9f5105a0c13f6844db89a80a7e
Hannes <simesgaarden@hotmail.com>
2018-05-04 10:13:48 +0200

    Quick fix
    .

--- Assets/Scripts/NotesHuntHandler.cs
+++ Assets/Scripts/NotesHuntHandler.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 
 public class NotesHuntHandler : MonoBehaviour {
 
-    [SerializeField] private GameObject notePrefab;
+    [SerializeField] private GameObject correctNotePrefab;
+    [SerializeField] private GameObject incorrectNotePrefab;
     [SerializeField] private int noteAmount;
     [SerializeField] private float speed;
     [Tooltip("Minimum x spawn coordinate")][SerializeField] private float xSpawnRandomMin;
     [Tooltip("Maximum x spawn coordinate")][SerializeField] private float xSpawnRandomMax;
     [Tooltip("Interval in second that notes will spawn in")][SerializeField] private float spawnInterval;
     [SerializeField] private float rayLength = 100;
+    [SerializeField]
+
+    /* 
+        public enum NoteType { CORRECT, INCORRECT };
+        public NoteType noteType { get; set; }
+        // First value represents the spawn time in milliseconds and the second value represents the type of note that will be spawned
+        public List<KeyValuePair<int, NoteType>> noteSpawns; //= new List<KeyValuePair<int, NoteType>>(); */
 
     public enum NoteType { CORRECT, INCORRECT };
-    public NoteType noteType { get; set; }
-    // First value represents the spawn time in milliseconds and the second value represents the type of note that will be spawned
-    public List<KeyValuePair<int, NoteType>> noteSpawns; //= new List<KeyValuePair<int, NoteType>>();
+
+    [System.Serializable]
+    public class Notes
+    {
+        public NoteType noteType;
+        [Tooltip("Spawntime in milliseconds relative to the song")] public int spawnTime;
+        [HideInInspector] public int points;  // The points that the player recieves when destroying the object. Public to be able to use multipliers for powerups, etc
+    }
+
+    public List<Notes> notesList;
 
     void Start ()
     {
-        
-	}
+        StartCoroutine(SpawnCooldown());
+    }
 	
 	
 	void Update ()
@@ -61,9 +76,8 @@ public class NotesHuntHandler : MonoBehaviour {
     {
 
         // Spawn the note and set the speed
-        GameObject note = Instantiate(notePrefab, new Vector3(Random.Range(xSpawnRandomMin, xSpawnRandomMax), 0), Quaternion.identity);
+        GameObject note = Instantiate(correctNotePrefab, new Vector3(Random.Range(xSpawnRandomMin, xSpawnRandomMax), 0), Quaternion.identity);
         note.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.up * speed);
-
     }
 
     private IEnumerator SpawnCooldown()
