ee27ce50d219cc00579f98544672b7d2e3ce93c7
Henrik <a16henle@student.his.se>
2018-05-09 21:35:09 +0200

    BookOfLies added
    Added early draft of logbook system

--- Assets/Scripts/UI/StoryTelling.cs
+++ Assets/Scripts/UI/StoryTelling.cs
@@ -3,49 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
     [System.Serializable]
     class pages
 {
-    [TextArea(3, 10)]
+    [TextArea(3, 15)]
     public string pageText;
 }
 
     [System.Serializable]
     class episodes
 {
-    [SerializeField] public List<pages> pages;
+    public Button episodeButton;
+    public List<pages> pages;
 }
 
 public class StoryTelling : MonoBehaviour {
 
+    [SerializeField] private GameObject episodeMenu;
+    [SerializeField] private GameObject forwardButton;
+    [SerializeField] private GameObject backwardsButton;
     [SerializeField] private Text leftPage;
     [SerializeField] private Text rightPage;
     [SerializeField] private List<episodes> episodes;
+    private int episodeIndex = 0;
+    private int pageIndex = 0;
 
 
-    void Update()
+    public void InitializeStory(int index)
     {
-        if (Input.GetKeyDown(KeyCode.K))
-            FlipPagesForward();
-        if (Input.GetKeyDown(KeyCode.J))
-            FlipPagesBackwards();
+        episodeIndex = index;
+        episodeMenu.SetActive(false);
+        forwardButton.SetActive(true);
+        backwardsButton.SetActive(true);
+        pageIndex = 0;
+        if (episodes[index].pages.Count > 0)
+            leftPage.text = episodes[index].pages[0].pageText;
+        if (episodes[index].pages.Count > 1)
+            rightPage.text = episodes[index].pages[1].pageText;
     }
 
-    private void InitializeStory()
+    public void FlipPagesForward()
     {
-
+        if (pageIndex <= episodes[episodeIndex].pages.Count - 3)
+        {
+            // Hej Jack! Ljudeffekt här
+            pageIndex += 2;
+                leftPage.text = episodes[episodeIndex].pages[pageIndex].pageText;
+            if (pageIndex <= episodes[episodeIndex].pages.Count - 2)
+                rightPage.text = episodes[episodeIndex].pages[pageIndex + 1].pageText;
+            if (pageIndex == episodes[episodeIndex].pages.Count - 1)
+                rightPage.text = "";
+        }
     }
 
-    private void FlipPagesForward()
+    public void FlipPagesBackwards()
     {
-        leftPage.text = episodes[0].pages[2].pageText;
-        rightPage.text = episodes[0 ].pages[3].pageText;
+        if (pageIndex >= 1)
+        {
+            // Hej Jack! Ljudeffekt här
+            pageIndex -= 2;
+            leftPage.text = episodes[episodeIndex].pages[pageIndex].pageText;
+            rightPage.text = episodes[episodeIndex].pages[pageIndex + 1].pageText;
+        }
     }
 
-    private void FlipPagesBackwards()
+    public void BackToEpisodes()
     {
-        leftPage.text = episodes[0].pages[0].pageText;
-        rightPage.text = episodes[0].pages[1].pageText;
+        leftPage.text = "";
+        rightPage.text = "";
+        episodeMenu.SetActive(true);
+        forwardButton.SetActive(false);
+        backwardsButton.SetActive(false);
     }
 
 
