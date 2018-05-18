045756e6b7ada23e82635e267f2c5b806ab08ab6
Jung-bin <marcus.lagstrom@gmail.com>
2018-05-18 14:50:20 +0200

    Credits fix
    Quicker than quickest fix

---
+++ Assets/AutoStartSong.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoStartSong : MonoBehaviour {
+
+    public GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+        gameManager.audioManager.AudioPlayMusic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
