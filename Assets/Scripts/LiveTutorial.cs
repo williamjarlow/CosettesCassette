5ead7dc100d76de575602e836ed40f1ef011e0e0
Henrik <a16henle@student.his.se>
2018-05-22 10:52:38 +0200

    Ripped Tutorial asset apart
    Separated Tutorial system and Live Tutorial system.

---
+++ Assets/Scripts/LiveTutorial.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class LiveTutorials
+{
+    [TextArea]
+    [SerializeField]
+    public string tutorialText;
+    public float timeToAppearInMs;
+    public bool hasBeenShown;
+}
+
+public class LiveTutorial : MonoBehaviour {
+
+
+    [SerializeField] private GameObject liveTutorial;
+    [SerializeField] private Text textBoxForLiveTutorial;
+
+    [SerializeField] public LiveTutorials[] liveTutorials;
+
+    private AudioManager audioManager;
+
+
+    void Start ()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+	}
+
+    void Update()
+    {
+        if (liveTutorials.Length > 0)
+        {
+            for (int i = 0; i < liveTutorials.Length; i++)
+            {
+                if (liveTutorials[i].timeToAppearInMs >= audioManager.GetTimeLinePosition() - 30 && liveTutorials[i].timeToAppearInMs <= audioManager.GetTimeLinePosition() + 30 && liveTutorials[i].hasBeenShown == false)
+                {
+                    OpenLiveTutorial();
+                    textBoxForLiveTutorial.gameObject.SetActive(true);
+                    textBoxForLiveTutorial.text = liveTutorials[i].tutorialText;
+                    liveTutorials[i].hasBeenShown = true;
+                }
+            }
+        }
+    }
+
+
+    private void OpenLiveTutorial()
+    {
+        liveTutorial.SetActive(true);
+        audioManager.AudioPauseMusic();
+        audioManager.pausedMusic = true;
+    }
+
+    public void CloseLiveTutorial()
+    {
+        liveTutorial.SetActive(false);
+        audioManager.AudioUnpauseMusic();
+        audioManager.pausedMusic = false;
+    }
+
+
+
+
+}
