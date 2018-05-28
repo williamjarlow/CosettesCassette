48a5d3d3ee4ef88cfef8d7331e0673eb3ab07dbc
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2018-05-28 13:26:32 +0200

    bup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -108,7 +108,7 @@ public class GameManager : MonoBehaviour
                 // Find and jump to the closest segment
                 SnapToClosestSegment();
 
-                yield return new WaitForSeconds(tolerance / 1000);
+                yield return new WaitForSeconds(tolerance/1000);
 
                 // Start recording
                 recording = true;
@@ -132,7 +132,7 @@ public class GameManager : MonoBehaviour
 
     public void ToggleRecord()
     {
-        
+        StartCoroutine(ToggleRecordCoroutine());
     }
     public void SetRecord(bool recordingState)
     {
