1ee785189ac1c3a6088e5303248a8d80688201dd
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2018-06-07 16:12:51 +0200

    Cleanup

--- Assets/Scripts/AudiologPicture.cs
+++ Assets/Scripts/AudiologPicture.cs
@@ -8,12 +8,9 @@ public class AudiologPicture : MonoBehaviour {
 
     [SerializeField] private Image AudiologPic;
     [HideInInspector] public float currentPos;
-    private float trackLength;
-    private GameObject AudioManager;
     [Tooltip("Time in milliseconds from start of audiolog to when picture appears.")]
     [SerializeField] private float pictureAppearTime;
     private AudioManager audioManager;
-    private float audiolength;
     private float audiologlength;
     private float audioPos;
     [HideInInspector] public bool showpicture = false;
@@ -23,8 +20,6 @@ public class AudiologPicture : MonoBehaviour {
     {
 
         audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
-        audiolength = audioManager.GetTrackLength();
-        trackLength = audioManager.GetTrackLength();
 
         AudiologPic.CrossFadeAlpha(0, 0.0f, false);
         AudiologPic.enabled = false;
@@ -50,8 +45,7 @@ public class AudiologPicture : MonoBehaviour {
 
         if (audioManager.switchedToAudioLog == true)
         {
-            //transform.localPosition = new Vector3(transform.localPosition.x, audioManager.GetTimeLinePosition(), transform.localPosition.z);
-            if (audioManager.GetTimeLinePosition() > pictureAppearTime && showpicture == false)
+           if (audioManager.GetTimeLinePosition() > pictureAppearTime && showpicture == false)
             {
                 Fade();
                 showpicture = true;
